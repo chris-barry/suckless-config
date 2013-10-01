82d8109d8206e929fe6cb336e54d447a29ade486
Chris Barry <chris@barry.im>
2013-09-30 21:03:57 -0400

    Switching some spaces to tabs

--- dwmstatus/dwmstatus.c
+++ dwmstatus/dwmstatus.c
@@ -67,29 +67,29 @@ get_time(char *tzname)
 char*
 get_bat()
 {
-    long  lnum1, lnum2 = 0;
-    char* status = malloc(sizeof(char)*12);
-    char  s = '?';
-    FILE* fp = NULL;
+	long  lnum1, lnum2 = 0;
+	char* status = malloc(sizeof(char)*12);
+	char  s = '?';
+	FILE* fp = NULL;
 
-    if ((fp = fopen(BATT_NOW, "r"))) {
-        fscanf(fp, "%ld\n", &lnum1);
-        fclose(fp);
-        fp = fopen(BATT_FULL, "r");
-        fscanf(fp, "%ld\n", &lnum2);
-        fclose(fp);
-        fp = fopen(BATT_STATUS, "r");
-        fscanf(fp, "%s\n", status);
-        fclose(fp);
-        if (strcmp(status,"Charging") == 0)
-            s = '+';
-        if (strcmp(status,"Discharging") == 0)
-            s = '-';
-        if (strcmp(status,"Full") == 0)
-            s = '=';
-        return smprintf("[%c%ld%%]", s,(lnum1/(lnum2/100)));
-    }
-    else return smprintf("");
+	if ((fp = fopen(BATT_NOW, "r"))) {
+		fscanf(fp, "%ld\n", &lnum1);
+		fclose(fp);
+		fp = fopen(BATT_FULL, "r");
+		fscanf(fp, "%ld\n", &lnum2);
+		fclose(fp);
+		fp = fopen(BATT_STATUS, "r");
+		fscanf(fp, "%s\n", status);
+		fclose(fp);
+		if (strcmp(status,"Charging") == 0)
+			s = '+';
+		if (strcmp(status,"Discharging") == 0)
+			s = '-';
+		if (strcmp(status,"Full") == 0)
+			s = '=';
+		return smprintf("[%c%ld%%]", s,(lnum1/(lnum2/100)));
+	}
+	else return smprintf("");
 }
 
 char*
@@ -124,7 +124,7 @@ get_vol()
 		return smprintf("");
 	}
 	
-    sscanf(result, "%i", &volume);
+	sscanf(result, "%i", &volume);
 	return smprintf("[%ddB]", volume);
 }
 
